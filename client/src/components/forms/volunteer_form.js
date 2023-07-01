6231f4b76725218b6f32050ad6eba85fe28df999
Riddhi <riddhichaudhary15@gmail.com>
2023-07-02 00:30:21 +0530

    Added Login page and fixed student registration page css

--- client/src/components/forms/volunteer_form.js
+++ client/src/components/forms/volunteer_form.js
@@ -43,7 +43,7 @@ function volunteerForms() {
                     </div>
 
                     <div class="md:col-span-3">
-                      <label for="address">Address / Street</label>
+                      <label for="address">Address</label>
                       <input
                         type="text"
                         name="address"
@@ -54,52 +54,7 @@ function volunteerForms() {
                       />
                     </div>
 
-                    <div class="md:col-span-2">
-                      <label for="state">State / province</label>
-                      <div class="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
-                        <input
-                          name="state"
-                          id="state"
-                          placeholder="State"
-                          class="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
-                          value=""
-                        />
-                        <button
-                          tabindex="-1"
-                          class="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600"
-                        >
-                          <svg
-                            class="w-4 h-4 mx-2 fill-current"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          >
-                            <line x1="18" y1="6" x2="6" y2="18"></line>
-                            <line x1="6" y1="6" x2="18" y2="18"></line>
-                          </svg>
-                        </button>
-                        <button
-                          tabindex="-1"
-                          for="show_more"
-                          class="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600"
-                        >
-                          <svg
-                            class="w-4 h-4 mx-2 fill-current"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          >
-                            <polyline points="18 15 12 9 6 15"></polyline>
-                          </svg>
-                        </button>
-                      </div>
-                    </div>
+                    
                     <div class="md:col-span-2">
                       <label for="highest-qualification">
                         Highest Qualification:
