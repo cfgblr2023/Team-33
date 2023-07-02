1a270aef4ada60008642e85e4e0da517db7706f9
Shruti Singh <gshrutisingh@gmail.com>
2023-07-02 07:13:58 +0530

    Admin integration

--- client/src/components/admin_dashboard/CreateEvent.js
+++ client/src/components/admin_dashboard/CreateEvent.js
@@ -1,13 +1,71 @@
-import React from 'react'
-import Layout from './Layout.js';
-import {useState} from 'react';
+import React, { useEffect } from "react";
+import Layout from "./Layout.js";
+import { useState } from "react";
+import moment from "moment";
 
 function CreateEvent() {
-    const [title,setTitle]=useState("");
-    const [mode, setMode]=useState("");
-    return (
-        <>
-        <Layout/>
+  const [title, setTitle] = useState("");
+  const [formData, setFormData] = useState({
+    skill: "",
+    online: "false",
+    date_and_time: "",
+    link: "",
+    location: "",
+  });
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    const getSkills = () => {
+      fetch(`${process.env.REACT_APP_URL}skills`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setSkills(data);
+        })
+        .catch((err) => {
+          console.log(err.message);
+          setSkills([]);
+        });
+    };
+    getSkills();
+  }, []);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    fetch(`${process.env.REACT_APP_URL}events/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        skill: formData.skill,
+        online: formData.online,
+        date_and_time: moment(formData.date_and_time).toISOString(),
+        link: formData.link,
+        loaction: formData.location,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  return (
+    <>
+      <Layout />
       <div className="App">
         <div class="min-h-screen p-6 bg-white">
           <div class="container max-w-screen-lg mx-auto">
@@ -18,109 +76,90 @@ function CreateEvent() {
                     <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5 text-left">
                       <div class="md:col-span-5">
                         <label for="full_name">Event title</label>
+                        {skills.map((play, i) => (
+                          <select
+                            name="highest-qualification"
+                            id="highest-qualification"
+                            class="text-gray-800 w-full h-10 bg-gray-50 flex border border-gray-200 rounded items-center"
+                            key="qa"
+                            value={formData.skill}
+                            onChange={(e) =>
+                              this.setForm({ value: e.target.value })
+                            }
+                          >
+                            <option value="No formal education" key="formal">
+                              {play.name}
+                            </option>
+                          </select>
+                        ))}
+                      </div>
+                      <label>
+                        <input
+                          type="checkbox"
+                          value="true"
+                          onChange={handleChange}
+                        />
+                        Online
+                      </label>
+                      <div>
+                        <label for="event_mode">Date</label>
+                        <div class="relative max-w-sm ">
+                          <div class="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none"></div>
+                          <input
+                            value={formData.date_and_time}
+                            onChange={handleChange}
+                            datepicker
+                            type="date"
+                            class="bg-white border border-black text-black-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-8 p-2 dark:bg-white-700"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <label class="md:col-span-2" for="link">
+                          Location
+                        </label>
                         <input
                           type="text"
-                          name="full_name"
-                          id="full_name"
+                          name="location"
+                          id="location"
                           class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                          value={title}
-                          onChange={(e)=>{setTitle(e.target.value)}}
+                          value={formData.location}
+                          onChange={handleChange}
                         />
                       </div>
-                    
-                    <div>
-                      <label for="event_mode">Mode</label>
-                      <ul class="w-30 items-text-sm font-medium text-black text-black-900 bg-white border border-black-200 rounded-lg">
-                        <li class="w-full border-b border-black-200 rounded-t-lg dark:border-black">
-                            <div class="flex items-center pl-3">
-                                <input id="list-radio-license" type="radio" value="" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-black-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-black-500"/>
-                                <label for="list-radio-license" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-black-300">Online</label>
-                            </div>
-                        </li>
-                        <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                            <div class="flex items-center pl-3">
-                                <input id="list-radio-id" type="radio" value="" name="list-radio" class="w-4 h-4 text-blue-600 bg-black-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-black-500"/>
-                                <label for="list-radio-id" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-black-300">Offline</label>
-                            </div>
-                        </li>
-                        <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                            <div class="flex items-center pl-3">
-                                <input id="list-radio-millitary" type="radio" value="" name="list-radio" class="w-4 h-4 text-blue-600 bg-black-100 border-black-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-black-500"/>
-                                <label for="list-radio-millitary" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-black-300">Hybrid</label>
-                            </div>
-                        </li>
-                        </ul>
-                        </div>
-                            <br/>
-
-                           <div>
-                            <label for="event_mode">Date</label>
-                            <div class="relative max-w-sm ">
-                                <div class="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none"></div> 
-                                <input datepicker type="date" class="bg-white border border-black text-black-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-8 p-2 dark:bg-white-700"/> 
-                            </div>
-                            </div>
-            
-            <br/>
-            
-
-            <div>
-            <label for="link">Location</label>
-            <input type="text" name="link" id="link" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value=""/>
-            </div>
-                      <div class="md:col-span-2">
-                        <label for="state">State / province</label>
-                        <div class="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
-                          <input
-                            name="state"
-                            id="state"
-                            placeholder="State"
-                            class="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
-                            value=""
-                          />
-                          <button
-                            tabindex="-1"
-                            class="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600"
-                          >
-                            <svg
-                              class="w-4 h-4 mx-2 fill-current"
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                              stroke-width="2"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            >
-                              <line x1="18" y1="6" x2="6" y2="18"></line>
-                              <line x1="6" y1="6" x2="18" y2="18"></line>
-                            </svg>
-                          </button>
-                          <button
-                            tabindex="-1"
-                            for="show_more"
-                            class="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600"
-                          >
-                            <svg
-                              class="w-4 h-4 mx-2 fill-current"
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                              stroke-width="2"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            >
-                              <polyline points="18 15 12 9 6 15"></polyline>
-                            </svg>
-                          </button>
-                        </div>
-                      </div>
-                      
                       <div>
-            <label class="md:col-span-2" for="link">Link</label>
-            <input type="text" name="link" id="link" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value=""/>
-            </div>
-
-                      
+                        <label class="md:col-span-2" for="link">
+                          Link
+                        </label>
+                        <input
+                          type="text"
+                          name="link"
+                          id="link"
+                          class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                          value={formData.link}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <a
+                        href="/volunteer_register"
+                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 button"
+                        onClick={handleSubmit}
+                      >
+                        Register
+                        <svg
+                          aria-hidden="true"
+                          class="w-4 h-4 ml-2 -mr-1"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                          ></path>
+                        </svg>
+                      </a>
                     </div>
                   </div>
                 </div>
@@ -129,9 +168,8 @@ function CreateEvent() {
           </div>
         </div>
       </div>
-      </>
-    );
-  }
-  
-  export default CreateEvent;
-  
+    </>
+  );
+}
+
+export default CreateEvent;
